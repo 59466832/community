9a24ddd25666bebd19372a0df2dbe6eead74e8a7
59466832 <59466832@qq.com>
2020-02-27 22:11:59 +0800

    cloud not autowrited ? @Component(value = userMapper)

--- src/main/java/com/xincdata/community/community/mapper/UserMapper.java
+++ src/main/java/com/xincdata/community/community/mapper/UserMapper.java
@@ -3,8 +3,10 @@ package com.xincdata.community.community.mapper;
 import com.xincdata.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component(value = "userMapper")
 public interface UserMapper {
     @Insert("insert into user (name, token, account_id, gmt_create, gmt_modified) values (#{name}, #{token}, #{accountId}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
